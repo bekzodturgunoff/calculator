12a4d1c48d627cf6d6f59fb341d5cbefceeaafda
Bekzod <60348530+bekzodturgunoff@users.noreply.github.com>
2022-01-21 11:38:42 +0500

    hell

--- script.js
+++ script.js
@@ -1,19 +1,86 @@
+class Calculator {
+  constructor(lastDisplay, currentDisplay) {
+    this.lastDisplay = lastDisplay
+    this.currentDisplay = currentDisplay
+    this.clear()
+  }
+  clear() {
+    this.lastDisplay.innerText = ""
+    this.currentDisplay.innerText = ""
+    this.operator = undefined
+  }
+
+  delete() {
+
+  }
+
+  appendNumber(number) {
+    if (number === "." && currentDisplay.innerText.includes(".")) return
+    currentDisplay.innerText = currentDisplay.innerText + number
+
+  }
+
+
+  chooseOperation(operation) {
+    this.operation = operation
+    this.lastDisplay = this.currentDisplay
+    this.currentDisplay.innerText = ""
+
+
+  }
+
+  compute() {
+
+  }
+
+  upDateDisplay() {
+    this.currentDisplay = this.currentDisplay
+
+
+  }
+
+}
+
 const allNumbers = document.querySelectorAll(".numbers")
 const clearBtn = document.querySelector(".btn-clear")
 const deleteBtn = document.querySelector(".btn-delete")
-const operator = document.querySelectorAll("[data-operator]")
+let operator = document.querySelectorAll("[data-operator]")
 const numbers = document.querySelectorAll("[data-number]")
-const currentDisplay = document.querySelector("#current-screen")
-const lastDisplay = document.querySelector("#last-screen")
+const currentDisplay = document.querySelector("[data-current-screen]")
+const lastDisplay = document.querySelector("[data-last-screen]")
 const equal = document.querySelector('#equal-btn')
 const dot = document.querySelector("#dot-btn")
+const calculator = new Calculator(lastDisplay, currentDisplay)
+
+numbers.forEach(button => {
+  button.addEventListener("click", () => {
+    calculator.appendNumber(button.innerText)
+    calculator.upDateDisplay()
+  })
+})
 
 
-allNumbers.forEach(number => number.addEventListener("click", () => {
-  console.log("pressed")
-}))
+operator.forEach(operation => {
+  operation.addEventListener("click", () => {
+    calculator.chooseOperation(operation.innerText)
+    calculator.upDateDisplay()
+  })
+})
 
+clearBtn.addEventListener("click", () => {
+  calculator.clear()
+})
 
+// equal.addEventListener("click", () => {
+//   if (currentDisplay.innerText.includes("+")) return
+//   if (currentDisplay.innerText.includes("-")) return
+//   if (currentDisplay.innerText.includes("×")) return
+//   if (currentDisplay.innerText.includes("÷")) return
+//   add(currentDisplay.innerText, lastDisplay.innerText)
+//   subtract(currentDisplay.innerText, lastDisplay.innerText)
+//   multiply(currentDisplay.innerText, lastDisplay.innerText)
+//   divide(currentDisplay.innerText, lastDisplay.innerText)
+// })
 
 function add(numberOne, numberTwo) {
   return numberOne + numberTwo
